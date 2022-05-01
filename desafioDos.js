491e3b4cd5b637f77f5a33c574f16d16f692b4b7
Nicolas Clementi <clementinicolas@gmail.com>
2022-05-01 16:49:35 -0300

    initial desafioDos

---
+++ desafioDos.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const { stringify } = require('querystring');
+
+
+class contenedor{
+    constructor(FileName){
+        this.FileName=FileName;
+        this.products=[]
+        this.id=0
+        
+    }
+    
+    save= async (product)=>{
+        this.id=this.id +1
+        product= {
+            ...product,
+            id:this.id
+        }
+        this.products.push(product)
+        try{
+          
+            await fs.promises.writeFile(`./${this.FileName}.txt`,JSON.stringify(this.products, null, '\t'))
+            
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    getById= async(id)=>{
+
+    }
+    getAll= async()=>{
+
+    }
+    deleteById= async(id)=>{
+
+    }
+    deleteAll= async()=>{
+
+    }
+
+}
+
+
+productExample={
+    title:"prueba",
+    price:100,
+    thumbnail:"https://source.unsplash.com/random"
+}
+
+
+const pokemones=[]
+const prueba = new contenedor("prueba1")
+
+prueba.save(productExample)
+
+prueba.save(productExample)
+
+prueba.save(productExample)
+prueba.save(productExample)
+prueba.save(productExample)
+prueba.save(productExample)
+prueba.save(productExample)
+
+
+
+
