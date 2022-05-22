13608431592d11934e3bcea3d85e3b4c946c4b0a
Nicolas Clementi <clementinicolas@gmail.com>
2022-05-21 23:44:41 -0300

    inicio d5  hbs

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express=require('express')
 const path = require('path')
 const app=express()
 const PORT = 8080;
+const handlebars = require('express-handlebars')
+
 
 
 const routersProducts = require('./routes/ProductRoute')
-const routersMain = require('./routes/MainRoute')
+const routersMain = require('./routes/MainRoute');
+const {readJson} = require('./controller/helpers');
 
 
 express.static(path.resolve(__dirname, './public'))
@@ -13,7 +16,25 @@ express.static(path.resolve(__dirname, './public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+//plantilla hbs
+app.engine(
+"hbs",
+handlebars.engine({
+    extname:'.hbs',
+    defaultLayout:'index.hbs',
+    layoutsDir:__dirname+'/views/layouts'
 
+
+})
+)
+
+app.set('view engine','hbs');
+app.set('views','./views')
+
+app.get('/productos',async (req,res)=>{
+    const productos=    await readJson('db')    
+    res.render('main',{productos})
+})
 app.use('/',routersMain)
 
 
